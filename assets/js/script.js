49ea6b63a4ec6e7972ade3632cd45bddbe362c08
walker <spadonival7@gmail.com>
2024-05-30 10:57:29 -0400

    assighned api to a variable

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function initPage() {
+    const cityEl = document.getElementById("enter-city");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    var fivedayEl = document.getElementById("fiveday-header");
+    var todayweatherEl = document.getElementById("today-weather");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+    // Assigning my APi to a variable useing my api key
+    const APIKey = "0d7a6fae6b37c79c3fa2ccbbbd43b680";
